591c63111bfd9723cde13d77f9a922f81393ea8c
DineshReddy1812 <dineshgowravarapu218@gmail.com>
2025-05-13 12:39:02 +0530

    project page rectangles

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -3,11 +3,29 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div className="bg-orange-100 flex " style={{ minHeight: 'calc(100vh - 82px)' , backgroundColor: 'rgba(255, 138, 0, 0.1)' }}>
-      <div class="w-64 h-40 bg-white border border-gray-300 shadow-md rounded-lg p-4">
+    <div className="bg-orange-100 " style={{ minHeight: 'calc(100vh - 82px)' , backgroundColor: 'rgba(255, 138, 0, 0.1)' }}>
+      <div className="ml-135 pt-10 pb-10">
+      <h1 className="text-5xl font-bold text-gray-900">Featured Projects</h1>
+      </div>
+      <div class="w-230 h-100 ml-70  bg-white border border-gray-300 shadow-md rounded-lg p-4 mb-10">
   
-  <p class="text-gray-800">This is a white rectangle box</p>
-</div>
+      <p class="text-gray-800">This is a white rectangle box</p>
+      </div>
+
+      <div class="w-230 h-100 ml-70  bg-white border border-gray-300 shadow-md rounded-lg p-4 mb-10">
+  
+      <p class="text-gray-800">This is a white rectangle box</p>
+      </div>
+
+      <div class="w-230 h-100 ml-70  bg-white border border-gray-300 shadow-md rounded-lg p-4 mb-10">
+  
+      <p class="text-gray-800">This is a white rectangle box</p>
+      </div>
+
+      <div class="w-230 h-100 ml-70  bg-white border border-gray-300 shadow-md rounded-lg p-4 pb-30">
+  
+      <p class="text-gray-800">This is a white rectangle box</p>
+      </div>
     </div>
   )
 }
